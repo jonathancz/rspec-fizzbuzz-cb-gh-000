208acd8c70311c691fa058bd71c1a12a3a57d057
Ethan Chen <jonathanchenzheng@hotmail.com>
2018-06-26 00:11:49 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-    if number == % 3
-    puts ('Fizz')
+  if number == % 3
+    "Fizz"
   elsif number == % 5
-    puts ('Buzz')
+    "Buzz"
   elsif number == %15
-    puts ('FizzBuzz')
+    "FizzBuzz"
   elsif
     nil
   end
